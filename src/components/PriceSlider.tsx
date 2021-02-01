e1bcb1cf9a0b2041b9b490ce9773d6af5705e2ce
Alejandro <alejandro@plantjammer.com>
2021-02-01 17:14:57 +0100

    refactor to react 16.8 with typescript

--- src/components/PriceSlider.tsx
+++ src/components/PriceSlider.tsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
-//import logo from './logo.svg';
 import "./PriceSlider.css";
 import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
 
+const labelStyle = {
+  display: "block",
+  margin: "0 0 .28571429rem 0",
+  color: "rgba(0,0,0,.87)",
+  "font-size": " .92857143em",
+  "font-weight": "700",
+  "text-transform": "none",
+};
+
+const wrapperStyle = {
+  height: "100%",
+  display: "flex",
+  "flex-direction": "column",
+  "justify-content": "space-around",
+  "align-items": "center",
+};
+
 const Range = Slider.Range;
 
-//const style = { width: '100%', padding: 20, display: 'flex', flexDirection: 'row', justifyContent: 'space-around' };
 type PriceRange = {
   myMin: number;
   myMax: number;
@@ -18,10 +33,10 @@ type PriceSliderProps = {
   max: number;
 };
 
-const PriceSlider = (props: PriceSliderProps) => {
+export default function(props: PriceSliderProps) {
   const [priceRange, setPriceRange] = useState<PriceRange>();
-  const [loaded, setLoaded] = useState(false);
-  const [steps, setSteps] = useState({});
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [steps, setSteps] = useState<{ [id: number]: string }>({});
 
   const { min, max, addPrice } = props;
 
@@ -44,25 +59,8 @@ const PriceSlider = (props: PriceSliderProps) => {
 
   useEffect(() => {
     getSteps();
-    addPrice({ myMin: priceRange.myMin, myMax: priceRange.myMax });
+    addPrice(priceRange);
   });
-
-  const labelStyle = {
-    display: "block",
-    margin: "0 0 .28571429rem 0",
-    color: "rgba(0,0,0,.87)",
-    "font-size": " .92857143em",
-    "font-weight": "700",
-    "text-transform": "none",
-  };
-
-  const wrapperStyle = {
-    height: "100%",
-    display: "flex",
-    "flex-direction": "column",
-    "justify-content": "space-around",
-    "align-items": "center",
-  };
 
   return (
     <div style={wrapperStyle}>
@@ -109,109 +107,4 @@ const PriceSlider = (props: PriceSliderProps) => {
       ) : null}
     </div>
   );
-};
-// class PriceSlider extends Component {
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//         isLoggedIn: false,
-//         priceRange: {
-//             myMin: this.min,
-//             myMax: this.max
-//         },
-//         loaded: false
-//     }
-// }
-// onSliderChange = (value) => {
-//     this.addPrice({ myMin: value[0], myMax: value[1] })
-//     this.setState({ priceRange: {myMin: value[0], myMax: value[1] }})
-// }
-// updateLoadState = () => {
-//     this.setState({loaded: true})
-//   }
-// getSteps = () => {
-//     let callback = (object) =>{
-//         this.setState({ marks: object, priceRange: {myMin: this.min, myMax: this.max}}
-//         ,this.updateLoadState)
-//     }
-//     let total = this.max - this.min;
-//     let portion = total / 10;
-//     let object = {}
-//     let min = this.min;
-//     object[min] = ''
-//     for (let i = 0; i < 10; i++) {
-//         min += portion;
-//         object[min] = '';
-//     }
-//     callback(object)
-// }
-// async componentDidMount() {
-//     await this.getSteps();
-//     this.addPrice({priceRange: {myMin: this.state.myMin, myMax: this.state.myMax}});
-// }
-// render() {
-//   const labelStyle = {
-//     display: "block",
-//     margin: "0 0 .28571429rem 0",
-//     color: "rgba(0,0,0,.87)",
-//     fontSize: " .92857143em",
-//     fontWeight: "700",
-//     textTransform: "none",
-//   };
-//   return (
-//     <div
-//       style={{
-//         height: "100%",
-//         display: "flex",
-//         flexDirection: "column",
-//         justifyContent: "space-around",
-//         alignItems: "center",
-//       }}
-//     >
-//       {this.state.loaded ? (
-//         <div>
-//           <label style={labelStyle}>Price range</label>
-//           <div
-//             style={{
-//               marginTop: "10px",
-//               display: "flex",
-//               flexDirection: "row",
-//               justifyContent: "space-around",
-//               alignItems: "center",
-//             }}
-//           >
-//             {this.state.priceRange.myMin}
-//             <Range
-//               marks={this.state.marks}
-//               allowCross={false}
-//               step={1}
-//               min={this.min}
-//               max={this.max}
-//               defaultValue={[this.min, this.max]}
-//               style={{
-//                 minWidth: "300px",
-//                 width: "auto",
-//                 marginLeft: 20,
-//                 marginRight: 20,
-//               }}
-//               onChange={this.onSliderChange}
-//               trackStyle={[
-//                 { backgroundColor: "white" },
-//                 { backgroundColor: "white" },
-//               ]}
-//               handleStyle={[
-//                 { backgroundColor: "#99ff99" },
-//                 { backgroundColor: "#ffcc00" },
-//               ]}
-//               railStyle={{ backgroundColor: "gray" }}
-//             />
-//             {this.state.priceRange.myMax}
-//           </div>
-//         </div>
-//       ) : null}
-//     </div>
-//   );
-// }
-// }
-
-export default PriceSlider;
+}
